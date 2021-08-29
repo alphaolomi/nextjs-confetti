90492d605bc9d537d54794dc6efc2e37f4611060
Alpha Olomi <alphaolomi@gmail.com>
2021-08-29 09:57:46 +0300

    make confeti stop

--- src/Confetti.tsx
+++ src/Confetti.tsx
@@ -7,12 +7,13 @@ const conf = () => {
   return (
     <div>
       <Confetti
+      
         width={width}
         height={height}
         opacity={0.7} /* default is 1 */
-        // numberOfPieces={200} /*  default is 200 */
-        // recycle={false} /* Keep spawning confetti after numberOfPieces pieces have been shown. default is true*/
-        // onConfettiComplete={() => {console.log("Wow!! Confetti is done")}}
+        numberOfPieces={200} /*  default is 200 */
+        recycle={false} /* Keep spawning confetti after numberOfPieces pieces have been shown. default is true*/
+        onConfettiComplete={() => {console.log("Wow!! Confetti is done")}}
       />
     </div>
   );
